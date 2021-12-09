1125a8c656f67f4807707a4cdad8a5c1ceb2e3ab
hsloan <ishaqsloan@gmail.com>
2021-12-09 21:44:38 +0000

    Complete Raw Swap tools used in obelisk project

---
+++ plutus-use-cases/scripts/BruteSwap.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as T
+import qualified Data.Text.Read             as T
+import qualified Plutus.Contracts.RawSwap   as PCR
+import           System.Environment
+
+import           Ledger
+import           Ledger.Value
+import           PlutusTx.Builtins.Internal
+
+
+main :: IO ()
+main = do
+  coinACurrencySymbol:tokenNameA:amountA:coinBCurrencySymbol:tokenNameB:amountB:poolStateDatumHash:cardanoCliExe:cardanoNodeSocket:uniswapAddress:testnetMagic:_ <- getArgs
+  let coinACurrencySymbol' :: CurrencySymbol = CurrencySymbol $ BuiltinByteString $ T.encodeUtf8 $ T.pack coinACurrencySymbol
+      coinBCurrencySymbol' :: CurrencySymbol = CurrencySymbol $ BuiltinByteString $ T.encodeUtf8 $ T.pack coinBCurrencySymbol
+      tokenNameA' :: TokenName = TokenName $ BuiltinByteString $ T.encodeUtf8 $ T.pack tokenNameA
+      tokenNameB' :: TokenName = TokenName $ BuiltinByteString $ T.encodeUtf8 $ T.pack tokenNameB
+      amountA' :: Integer = either (\_ -> 0) Prelude.fst $ T.decimal $ T.pack amountA
+      amountB' :: Integer = either (\_ -> 0) Prelude.fst $ T.decimal $ T.pack amountB
+  PCR.main coinACurrencySymbol' tokenNameA' amountA' coinBCurrencySymbol' tokenNameB' amountB' poolStateDatumHash cardanoCliExe cardanoNodeSocket uniswapAddress testnetMagic
+
--- plutus-use-cases/src/Plutus/Contracts/RawSwap.hs
+++ plutus-use-cases/src/Plutus/Contracts/RawSwap.hs
@@ -1,27 +1,54 @@
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Plutus.Contracts.RawSwap where
 
 import           Cardano.Api
 import           Cardano.Api.Shelley
+import           Codec.Serialise
 import           Control.Monad                     (when)
 import qualified Data.Aeson                        as Aeson
+import qualified Data.ByteString                   as BS
+import qualified Data.ByteString.Base16            as B16
 import qualified Data.ByteString.Lazy              as BSL
 import           Data.Maybe
 import qualified Data.Text                         as T
 import qualified Data.Text.Encoding                as T
+import           GHC.Generics
 import           Ledger
+import           Ledger.Scripts
 import           Ledger.Value
 import           Plutus.Contracts.Uniswap.OffChain
 import           Plutus.Contracts.Uniswap.Types
+import           PlutusCore.Data
 import           PlutusTx
 import           PlutusTx.Builtins.Internal
 import           Safe
 import           System.Directory
 import           System.Process
 
-main :: (CurrencySymbol,TokenName) -> Integer -> (CurrencySymbol, TokenName) -> Integer -> FilePath -> IO ()
-main (coinACurrencySymbol, tokenNameA) amountA (coinBCurrencySymbol, tokenNameB) amountB poolStateDatumHash = do
+data RecentPoolBalance = RecentPoolBalance
+  { pcA :: Integer
+  , pcB :: Integer
+  } deriving (Show, Generic)
+
+instance FromJSON RecentPoolBalance
+instance ToJSON RecentPoolBalance
+
+main
+  :: CurrencySymbol
+  -> TokenName
+  -> Integer
+  -> CurrencySymbol
+  -> TokenName
+  -> Integer
+  -> FilePath  -- file with pool state datum
+  -> FilePath  -- exe path to cardano-cli
+  -> FilePath  -- path to cardano-node node.socket
+  -> String    -- uniswap smart contract address
+  -> String    -- testnet magic number
+  -> IO ()
+main coinACurrencySymbol tokenNameA amountA coinBCurrencySymbol tokenNameB amountB poolStateDatumHash cardanoCliExe cardanoNodeSocket addr magic = do
   let coinA = mkCoin coinACurrencySymbol tokenNameA
       coinB = mkCoin coinBCurrencySymbol tokenNameB
       swapParams = SwapParams {
@@ -33,13 +60,6 @@ main (coinACurrencySymbol, tokenNameA) amountA (coinBCurrencySymbol, tokenNameB)
   if not (spAmountA swapParams > 0 && spAmountB swapParams == 0 || spAmountA swapParams == 0 && spAmountB swapParams > 0)
     then print ("exactly one amount must be positive" :: String)
     else do
-      -- (_, (oref, o, lp, liquidity)) <- findUniswapFactoryAndPool us (spCoinA swapParams) (spCoinB swapParams)
-      -- let poolScriptDataFromDatum = fromBuiltinData $ dataToBuiltinData $ toPlutusData poolStateDatumHash
-      -- mOutVal <- datumFromHash poolStateDatumHash
-        -- TODO: How to turn Datum to a Ledger.Value?
-      -- let outVal = view ciTxOutValue o
-      -- TODO: Should not read pool state from a file, it should be read from the node. Use datumFromHash and add helper functions to Plutus.Contracts.Uniswap... to read from node
-      -- and calculate swap results.
       rawJsonScriptData <- BSL.readFile poolStateDatumHash
       let eJsonScriptData :: Either String Aeson.Value = Aeson.eitherDecode rawJsonScriptData
       case eJsonScriptData of
@@ -59,18 +79,21 @@ main (coinACurrencySymbol, tokenNameA) amountA (coinBCurrencySymbol, tokenNameB)
                 Just uniswapDatum -> case uniswapDatum of
                   Factory _ -> print ("factory found instead of LiquidityPool" :: String)
                   Pool lp la -> do
+                    pwd <- getCurrentDirectory
+                    print $ "pwd: " ++ (show pwd)
                     -- Read stdout to get the balance of each tokens that makes of the liquidity pool
-                    (_, cliOut, _) <- readCreateProcessWithExitCode
+                    (_, cliOut, cliErr) <- readCreateProcessWithExitCode
                       (CreateProcess
                         {
                           cmdspec = RawCommand
-                            "/home/zigpolymath/cardano-cli"
-                            ["query", "utxo", "--address", "addr_test1wpr9r6r6q0wgydckagwh52kvw2fxwq3mc3mpcym7l6l9lzqgvz4f8", "--testnet-magic", "8"]
+                            cardanoCliExe
+                            ["query", "utxo", "--address", addr, "--testnet-magic", magic]
                         , cwd = Nothing
                         , env = Just $ [(("CARDANO_NODE_SOCKET_PATH" :: String)
-                                     ,("/home/zigpolymath/node.socket" :: String))]
+                                     ,cardanoNodeSocket)]
                         , std_in = Inherit
                         , std_out = Inherit
+                        , std_err = Inherit
                         , close_fds = False
                         , create_group = False
                         , delegate_ctlc = False
@@ -89,6 +112,11 @@ main (coinACurrencySymbol, tokenNameA) amountA (coinBCurrencySymbol, tokenNameB)
                           T.decodeUtf8 bsB
                         mNodeQueryResults = headMay $ filter (\str -> T.isInfixOf tkA str && T.isInfixOf tkB str) $ map T.pack $ lines cliOut
                         -- drop leading txHash and txIndex, keep balances of what is being held at the utxo handle
+                    print $ show cliOut
+                    print $ show cliErr
+                    print $ show tkA
+                    print $ show tkB
+                    print $ show mNodeQueryResults
                     case mNodeQueryResults of
                       Nothing -> print ("No utxo found for token pool" :: String)
                       Just nodeQueryResults -> do
@@ -114,6 +142,8 @@ main (coinACurrencySymbol, tokenNameA) amountA (coinBCurrencySymbol, tokenNameB)
                             when (outA == 0) $ print "no payout" -- TODO: should throw
                             return (oldA - outA, oldB + (spAmountB swapParams))
                         print $ "Estimated amount to returned after swap: " ++ (show $ oldA - newA)
+                        print $ "Estimated amount of newB is : " ++ (show newB)
+                        print $ "Estimated amount to returned after inverse swap: " ++ (show $ oldB - newB)
                         print $ "New liquidity balance for " ++ (show tokenNameA) ++ ": " ++ (show newA)
                         print $ "New liquidity balance for " ++ (show tokenNameB) ++ ": " ++ (show newB)
 
@@ -121,9 +151,14 @@ main (coinACurrencySymbol, tokenNameA) amountA (coinBCurrencySymbol, tokenNameB)
 
                         let redeemerUniswapAction :: UniswapAction
                             redeemerUniswapAction = Swap
-                        let redeemerCoder = fromPlutusData $ builtinDataToData $ toBuiltinData redeemerUniswapAction
+                        let redeemerPlutusData = serialise $ builtinDataToData $ toBuiltinData redeemerUniswapAction
+                            redeemerCoder = fromPlutusData $ builtinDataToData $ toBuiltinData redeemerUniswapAction
                             redeemerJson = scriptDataToJson ScriptDataJsonDetailedSchema redeemerCoder
+                            recentPoolBalance = RecentPoolBalance (unAmount newA) (unAmount newB)
+                        print $ "redeemerCoder " ++ (show redeemerCoder)
+                        BSL.writeFile "./rawSwap/rawSwap-redeemer-test" $ BSL.fromStrict $ B16.encode $ BSL.toStrict redeemerPlutusData
                         BSL.writeFile "./rawSwap/rawSwap-redeemer" $ Aeson.encode redeemerJson
+                        BSL.writeFile "./rawSwap/recentPoolBalance.json" $ Aeson.encode recentPoolBalance
 
                         let poolScriptDataFromDatum = fromPlutusData $ builtinDataToData $ toBuiltinData uniswapDatum
                             poolScriptDataJson = scriptDataToJson ScriptDataJsonDetailedSchema poolScriptDataFromDatum
